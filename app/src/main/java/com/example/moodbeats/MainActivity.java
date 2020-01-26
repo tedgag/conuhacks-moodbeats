e6915f01edd5d1fb6dfbe61f508d639a5b333b24
GD <gindulink@gmail.com>
2020-01-25 21:07:51 -0500

    Added songs for each feeling (happy/sad/chill/stressed)

--- app/src/main/java/com/example/moodbeats/MainActivity.java
+++ app/src/main/java/com/example/moodbeats/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.moodbeats;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.media.MediaPlayer;
+import android.widget.Button;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaPlayer feeling;
+    Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        feeling = MediaPlayer.create(MainActivity.this, R.raw.slowdancing);
+        feeling.start();
     }
+
+
+
+
+
+
 }
