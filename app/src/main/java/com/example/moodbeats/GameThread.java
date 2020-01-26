6592ffcb10a7fe01dab3d5f6a6cb45f573aea15e
Edouard <tedgag@gmail.com>
2020-01-25 20:12:51 -0500

    Added base game loop files

---
+++ app/src/main/java/com/example/moodbeats/GameThread.java
@@ -0,0 +1,71 @@
+package com.example.moodbeats;
+
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+
+public class GameThread extends Thread {
+
+    private int FPS = 30;
+    private double avgFPS;
+    private SurfaceHolder surfaceHolder;
+    private Game game;
+    private boolean running;
+    private static Canvas canvas;
+
+    public GameThread(SurfaceHolder surfaceHolder, Game game){
+        super();
+        this.surfaceHolder = surfaceHolder;
+        this.game=game;
+    }
+
+    @Override
+    public void run() {
+        long startTime;
+        long timeMillis;
+        long waitTime;
+        long totalTime = 0;
+        int frameCount = 0;
+        long targetTime = 1000/FPS;
+        while (running){
+            startTime = System.nanoTime();
+            canvas = null;
+            try {
+                canvas = this.surfaceHolder.lockCanvas();
+                synchronized (surfaceHolder) {
+                    this.game.update();
+                    this.game.draw(canvas);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (canvas != null) {
+                    try {
+                        surfaceHolder.unlockCanvasAndPost(canvas);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            timeMillis = (System.nanoTime() - startTime)/1000000;
+            waitTime = targetTime - timeMillis;
+            try {
+                this.sleep(waitTime);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            totalTime += System.nanoTime() - startTime;
+            frameCount++;
+            if (frameCount == FPS) {
+                avgFPS = 1000/((totalTime/frameCount)/1000000);
+                frameCount = 0;
+                totalTime = 0;
+                System.out.println(avgFPS);
+            }
+
+        }
+    }
+    public void setRunning(boolean isRunning) {
+        running = isRunning;
+
+    }
+}
--- app/src/main/java/com/example/moodbeats/MainActivity.java
+++ app/src/main/java/com/example/moodbeats/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.moodbeats;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        // TODO: Switch to this on button press
+        // setContentView(new Game(this));
     }
 }
